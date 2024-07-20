62a3492391188acb8a6a3d53c588a7b73397f15f
vishal-koundal <vishal2661999@gmail.com>
2024-07-21 00:21:23 +0530

    added faq

---
+++ src/components/faq/Faq.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import FaqItem from './FaqItem'
+import Title from '@/elements/Title'
+
+const faqs = [1, 2, 3, 4, 5]
+const Faq = () => {
+  return (
+    <div className="mx-auto max-w-3xl md:my-24 my-16">
+      <div className="text-center mb-8">
+        <Title>Frequently Asked Questions</Title>
+      </div>
+      <div class="flow-root">
+        <div class=" divide-y divide-gray-100">
+          {faqs.map((item) => (
+            <FaqItem key={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Faq
