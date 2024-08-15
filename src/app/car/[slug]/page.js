a9d2919ba99523957990d99b9ae378af6a35044b
vishal-koundal <vishal2661999@gmail.com>
2024-08-15 14:33:05 +0530

    minor fixes

---
+++ src/app/car/[slug]/page.js
@@ -0,0 +1,17 @@
+import CarDetailsForm from '@/components/cars/CarDetailsForm';
+
+import Title from '@/elements/Title';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="container mx-auto md:px-6 px-4 md:py-16 py-10 min-h-screen">
+      <div className="text-center mb-4">
+        <Title>Car Details</Title>
+      </div>
+      full details here
+    </div>
+  );
+};
+
+export default page;
