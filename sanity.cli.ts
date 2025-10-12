e5917c3a367b0dd3098136b288381855eee2d19d
vishal-koundal <vishal2661999@gmail.com>
2025-10-12 13:48:54 +0530

    added app id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'c4b8gl55',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'beul4q7majtfiuk5zcl89wmb',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
-  }
+  },
 })
